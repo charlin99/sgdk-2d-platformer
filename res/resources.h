8fe8f6706fa9c7b619679dd02e7735c324a80933
cmiguelfilho <cmiguelsantosfilho@gmail.com>
2025-05-12 14:30:42 -0300

    Adding player and basic movement

--- res/resources.h
+++ res/resources.h
@@ -7,5 +7,6 @@ extern const Palette level_palette;
 extern const TileSet level_tileset;
 extern const TileSet level_map_tileset0;
 extern const MapDefinition level_map;
+extern const SpriteDefinition player_sprite;
 
 #endif // _RES_RESOURCES_H_
--- res/resources.res
+++ res/resources.res
@@ -1,3 +1,5 @@
 PALETTE level_palette "tileset.png"
 TILESET level_tileset "tileset.png" BEST ALL
 MAP level_map "level_test.tmx" LevelDesign BEST
+
+SPRITE player_sprite "player_sprite.png" 2 2 FAST
